fdccece8814a076ebf6a8e21d4d827fecdb07008
TaigerKala <124361451+TaigerKala@users.noreply.github.com>
2023-03-26 14:15:12 +0300

    Rename kodutoo1_yl2.cpp to kodutoo1_yl2_parem_lahendus.cpp

--- kodutoo1_yl2_parem_lahendus.cpp
+++ kodutoo1_yl2_parem_lahendus.cpp
@@ -1,5 +1,5 @@
-/*Team - homework t‰iendatud kood
-Koodi t‰iendas Taiger Kala*/
+/*Team - homework t√§iendatud kood
+Koodi t√§iendas Taiger Kala*/
 
 #include <iostream>
 using namespace std;
@@ -7,7 +7,7 @@ using namespace std;
 int nums[4] = { 2, 7, 11, 15 };
 int target = 26;
 
-//Tahame leida arrayst arvu "arv" ja "arv1" positsioonid, mille v‰‰rtuste summa on kokku 26.
+//Tahame leida arrayst arvu "arv" ja "arv1" positsioonid, mille v√§√§rtuste summa on kokku 26.
 int main() {
     for (int arv = 0; arv < size(nums); arv++) {
         for (int arv1 = 0; arv1 < size(nums); arv1++) {
